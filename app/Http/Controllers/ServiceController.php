1cc9793bcded1ffb75dbf7e0790843baf366b576
chiefkarim <krimotop2016@gmail.com>
2025-05-21 09:49:32 +0100

    feat: add service cread operations

---
+++ app/Http/Controllers/ServiceController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Service;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class ServiceController extends Controller
+{
+    public function index()
+    {
+        return Inertia::render('Services/Index', [
+            'services' => Service::all(),
+        ]);
+    }
+
+    public function create()
+    {
+        return Inertia::render('Services/Create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate(['name' => 'required']);
+        Service::create($request->only('name'));
+
+        return redirect()->route('services.index');
+    }
+
+    public function edit(Service $service)
+    {
+        return Inertia::render('Services/Edit', [
+            'service' => $service,
+        ]);
+    }
+
+    public function update(Request $request, Service $service)
+    {
+        $request->validate(['name' => 'required']);
+        $service->update($request->only('name'));
+
+        return redirect()->route('services.index');
+    }
+
+    public function destroy(Service $service)
+    {
+        $service->delete();
+
+        return redirect()->route('services.index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ServiceController;
+use App\Http\Controllers\ServiceRequestController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -10,6 +12,9 @@ Route::get('/', function () {
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::resource('services', ServiceController::class);
+Route::resource('service-requests', ServiceRequestController::class);
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
