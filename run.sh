dd4a0f4ea7691bf18d5ece0cdca8419922715b51
chiefkarim <krimotop2016@gmail.com>
2025-06-28 15:11:22 +0100

    feat: Add debugging output to run.sh for Render deployment

--- run.sh
+++ run.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
+set -e
+
+echo "--- Debugging run.sh ---"
+echo "APP_URL: $APP_URL"
+echo "SESSION_DOMAIN: $SESSION_DOMAIN"
+
+php artisan about --env
+php artisan config:show app.url
+php artisan config:show session.domain
+
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
+
 php-fpm &
 nginx -g 'daemon off;'
