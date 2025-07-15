e5d5b17b528b57d0276b5ff7566d55bdaa3ea52a
chiefkarim <krimotop2016@gmail.com>
2025-07-15 09:26:01 +0100

    chore: use env in cors

--- api/config/cors.php
+++ api/config/cors.php
@@ -19,8 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://laravel-service-app.onrender.com', 'https://laravel-service-app.vercel.app'],
-
+    'allowed_origins' => array_map('trim', explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:5173', 'http://127.0.0.1:5173', 'https://laravel-service-app.onrender.com', 'https://laravel-service-app.vercel.app'))),
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
